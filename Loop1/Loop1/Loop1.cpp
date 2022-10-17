72ab3daa50e3f8365542f3ccbaffb030478f4f4e
Roman Zbroiński <romanzbroinski@gmail.com>
2022-10-17 20:41:38 +0200

    task arrays 1
    Home work - task arrays, loop 1

---
+++ Loop1/Loop1/Loop1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    //First version
+   int liczby1[5];
+   for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j <= i; j++)
+            liczby1[i] = j;
+        cout << liczby1[i] << "\n";
+    }
+    return 0;
+}
