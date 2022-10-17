e44d9abeaa12dfeb1e036b0451b909f2aee639d2
Roman Zbroiński <romanzbroinski@gmail.com>
2022-10-17 21:55:54 +0200

    Home work - loop 2
    Home work - task arrays, loop 1

--- Loop1/Loop1/Loop1.cpp
+++ Loop1/Loop1/Loop1.cpp
@@ -3,12 +3,32 @@ using namespace std;
 int main()
 {
     //First version
-   int liczby1[5];
-   for (int i = 0; i < 5; i++)
+    constexpr size_t viele = 5;
+   int liczby1[viele];
+   for (int i = 0; i < viele; i++)
     {
-        for (int j = 0; j <= i; j++)
+        for (int j=0; j<= i; ++j)
             liczby1[i] = j;
-        cout << liczby1[i] << "\n";
+            cout << liczby1[i] << "\n";
     }
+
+   cout << endl << "******************" << endl;
+
+   constexpr size_t tab_size = 11;
+   int tab[tab_size];
+   for (size_t i = 0; i < tab_size; i++)
+   {
+       tab[i] = i;
+   }
+   for (size_t i = tab_size-1; i > 0; i--)
+   {
+       cout << tab[i]<<",";
+   }
+   cout << endl << "******************" << endl;
+
+   for (int a = 5; a >= 0;--a) {
+       cout << a << ", ";
+        }
+
     return 0;
 }
